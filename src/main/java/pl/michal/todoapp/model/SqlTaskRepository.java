e5aa899537e792a3a88e73b6a984409913623dab
Uzytkownik <uzytkownik@kursGIT.pl>
2021-05-20 23:05:15 +0200

    changes in SqlTaskRepository, override existsById method

--- src/main/java/pl/michal/todoapp/model/SqlTaskRepository.java
+++ src/main/java/pl/michal/todoapp/model/SqlTaskRepository.java
@@ -1,10 +1,15 @@
 package pl.michal.todoapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface SqlTaskRepository extends  TaskRepository, JpaRepository<Task,Integer> {
     //dzięki jpaRepository mamy dostęp do wielu użytecznych metod jak delete, save, find, count
 
+    @Override
+    @Query(nativeQuery = true, value = "select count(*) > 0 from tasks where id=:id")
+    boolean existsById(@Param("id") Integer id);
 }
