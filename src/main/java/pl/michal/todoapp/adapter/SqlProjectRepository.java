cba4400c74c30861eb53dd2a0b92cdbde3611f76
Uzytkownik <uzytkownik@kursGIT.pl>
2021-05-25 16:50:32 +0200

    added Project, ProjectStep and repositories, changes in relations for them, added V6__init_project_tables.sql

--- src/main/java/pl/michal/todoapp/adapter/SqlProjectRepository.java
+++ src/main/java/pl/michal/todoapp/adapter/SqlProjectRepository.java
@@ -1,19 +1,19 @@
 package pl.michal.todoapp.adapter;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import pl.michal.todoapp.model.Project;
+import pl.michal.todoapp.model.ProjectRepository;
 import pl.michal.todoapp.model.TaskGroup;
 import pl.michal.todoapp.model.TaskGroupRepository;
 
 import java.util.List;
 
 @Repository
-public interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
+public interface SqlProjectRepository extends ProjectRepository, JpaRepository<Project, Integer> {
     @Override
-    @Query("select distinct g from TaskGroup g join fetch g.tasks")
-    List<TaskGroup> findAll();
-    @Override
-    boolean existsByDoneIsFalseAndProject_Id(Integer groupId);
+    @Query("select distinct p from Project p join fetch p.steps")
+    List<Project> findAll();
+
 }
