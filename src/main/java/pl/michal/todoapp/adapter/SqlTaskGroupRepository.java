e48dd3881e2e611842f29275069b19bddb0f0cb8
Uzytkownik <uzytkownik@kursGIT.pl>
2021-05-25 16:02:05 +0200

    changes in findAll method in repo

--- src/main/java/pl/michal/todoapp/adapter/SqlTaskGroupRepository.java
+++ src/main/java/pl/michal/todoapp/adapter/SqlTaskGroupRepository.java
@@ -1,11 +1,17 @@
 package pl.michal.todoapp.adapter;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.michal.todoapp.model.TaskGroup;
 import pl.michal.todoapp.model.TaskGroupRepository;
 
+import java.util.List;
+
 @Repository
 public interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
-
+    @Override
+    @Query("from TaskGroup g join fetch g.tasks")
+    List<TaskGroup> findAll();
 }
