b740f888aeb6871b89d78f8c86a541bc344440ed
Uzytkownik <uzytkownik@kursGIT.pl>
2021-05-25 13:26:45 +0200

    added abstract class, which is extended by Task

---
+++ src/main/java/pl/michal/todoapp/model/BaseAuditableEntity.java
@@ -0,0 +1,22 @@
+package pl.michal.todoapp.model;
+
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+abstract class BaseAuditableEntity {
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+
+    @PrePersist
+    void prePersist(){
+        createdOn = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void preMerge(){
+        updatedOn = LocalDateTime.now();
+    }
+}
