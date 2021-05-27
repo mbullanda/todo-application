9e9e437cca7a274c14a9359386a4cef0101c8d53
mbull <mbullanda@gmail.com>
2021-05-27 12:36:08 +0200

    changes on Project, added TaskGroupService and methods

---
+++ src/main/java/pl/michal/todoapp/logic/TaskGroupService.java
@@ -0,0 +1,44 @@
+package pl.michal.todoapp.logic;
+
+import org.springframework.stereotype.Service;
+import pl.michal.todoapp.TaskConfigurationProperties;
+import pl.michal.todoapp.model.TaskGroup;
+import pl.michal.todoapp.model.TaskGroupRepository;
+import pl.michal.todoapp.model.TaskRepository;
+import pl.michal.todoapp.model.projection.GroupReadModel;
+import pl.michal.todoapp.model.projection.GroupWriteModel;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@Service
+public class TaskGroupService {
+
+    private TaskGroupRepository repository;
+    private TaskRepository taskRepository;
+
+    public TaskGroupService(final TaskGroupRepository repository, final TaskRepository taskRepository) {
+        this.repository = repository;
+        this.taskRepository = taskRepository;
+    }
+
+    public GroupReadModel createGroup(GroupWriteModel source){
+        TaskGroup result = repository.save(source.toGroup());
+        return new GroupReadModel(result);
+    }
+
+    public List<GroupReadModel> readAll(){
+        return repository.findAll().stream()
+                .map(GroupReadModel::new)
+                .collect(Collectors.toList());
+    }
+
+    public void toggleGroup(int groupId){
+        if (taskRepository.existsByDoneIsFalseAndGroup_Id(groupId)){
+            throw new IllegalStateException("Group has undone tasks. Done all the tasks first!");
+        }
+        TaskGroup result = repository.findById(groupId).orElseThrow(() -> new IllegalArgumentException("Task group with given id not found!"));
+        result.setDone(!result.isDone());
+    }
+}
