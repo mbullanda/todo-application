a52b0a23b9acebd5c950fc268ef626a8b63cfa6b
mbull <mbullanda@gmail.com>
2021-06-02 15:22:17 +0200

    changes in projects.html, created form

--- src/main/java/pl/michal/todoapp/controller/ProjectController.java
+++ src/main/java/pl/michal/todoapp/controller/ProjectController.java
@@ -1,14 +1,17 @@
 package pl.michal.todoapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.michal.todoapp.model.projection.ProjectWriteModel;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
     @GetMapping
-    String showProjects(){
+    String showProjects(Model model){
+        model.addAttribute("project", new ProjectWriteModel());
         return "projects";
     }
 }
