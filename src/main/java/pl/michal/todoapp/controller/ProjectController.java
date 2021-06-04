10738329e045c7275dd5a0cdba20fbc11c84c5d3
mbull <mbullanda@gmail.com>
2021-06-04 15:44:22 +0200

    changes in  validations

--- src/main/java/pl/michal/todoapp/controller/ProjectController.java
+++ src/main/java/pl/michal/todoapp/controller/ProjectController.java
@@ -2,6 +2,7 @@ package pl.michal.todoapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import pl.michal.todoapp.model.Project;
 import pl.michal.todoapp.model.ProjectStep;
 import pl.michal.todoapp.model.projection.ProjectWriteModel;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -30,7 +32,10 @@ public class ProjectController {
     }
 
     @PostMapping
-    String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model){
+    String addProject(@ModelAttribute("project") @Valid ProjectWriteModel current, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()){
+            return "projects";
+        }
         service.save(current);
         model.addAttribute("project", new ProjectWriteModel());
         model.addAttribute("message", "Dodano projekt!");
