1f9d810a3eed3195fb68ecaf36d14ad3d49ab544
mbull <mbullanda@gmail.com>
2021-06-10 14:52:18 +0200

    added around annotation to method in LogicAspect

--- src/main/java/pl/michal/todoapp/aspect/LogicAspect.java
+++ src/main/java/pl/michal/todoapp/aspect/LogicAspect.java
@@ -1,9 +1,32 @@
 package pl.michal.todoapp.aspect;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogicAspect {
+    private final Timer projectCreateGroupTimer;
+
+    LogicAspect(final MeterRegistry registry) {
+        projectCreateGroupTimer = registry.timer("logic.product.create.group");
+    }
+
+    @Around("execution(* pl.michal.todoapp.logic.ProjectService.createGroup(..))")
+    Object aroundProjectCreateGroup(ProceedingJoinPoint jp) {
+       return projectCreateGroupTimer.record(()-> {
+        try {
+            return jp.proceed();
+        } catch (Throwable throwable) {
+            if (throwable instanceof RuntimeException){
+                throw (RuntimeException)throwable;
+            }
+            throw new RuntimeException(throwable);
+        }
+    });
+    }
 }
