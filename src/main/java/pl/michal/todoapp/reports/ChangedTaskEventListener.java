fec416d1b82194c7569d111777e5f243deff57f1
mbull <mbullanda@gmail.com>
2021-06-15 13:51:05 +0200

    added PersistedTaskEvent and repo, new migration

--- src/main/java/pl/michal/todoapp/reports/ChangedTaskEventListener.java
+++ src/main/java/pl/michal/todoapp/reports/ChangedTaskEventListener.java
@@ -5,20 +5,31 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import pl.michal.todoapp.model.event.TaskDone;
+import pl.michal.todoapp.model.event.TaskEvent;
 import pl.michal.todoapp.model.event.TaskUndone;
 
 @Service
 class ChangedTaskEventListener {
     private static final Logger logger = LoggerFactory.getLogger(ChangedTaskEventListener.class);
+    private final PersistedTaskEventRepository repository;
+
+    ChangedTaskEventListener(final PersistedTaskEventRepository repository) {
+        this.repository = repository;
+    }
 
     @EventListener
     public void on(TaskDone event){
+        onChanged(event);
+    }
+
+    private void onChanged(final TaskEvent event) {
         logger.info("Got " + event);
+        repository.save(new PersistedTaskEvent(event));
     }
 
     @EventListener
     public void on(TaskUndone event){
-        logger.info("Got " + event);
+        onChanged(event);
     }
 
 }
